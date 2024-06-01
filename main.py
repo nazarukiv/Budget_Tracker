a7706c20ea8519067dfaef2075b060473d04a4e8
Ivan Nazaruk <nazaruk7649@ukr.net>
2024-06-01 18:27:28 +0100

    First Test Commit with Person class

---
+++ main.py
@@ -0,0 +1,5 @@
+class Person:
+    def __init__(self, name, current_budget, goal_budget) -> None:
+        self.name = name
+        self.current_budget = current_budget
+        self.goal_budget = goal_budget
